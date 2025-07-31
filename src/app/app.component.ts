16160ee8abe9f94bdee0c291b3ba31023c24f1fb
simonjoseph <simaojose1978@gmail.com>
2025-07-31 15:52:55 +0100

    make a certificate page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
+import { SecondaryButtonComponent } from './_components/secondary-button/secondary-button.component';
+import { ItemCertificadoComponent } from './_components/item-certificado/item-certificado.component';
+import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
+import { CertificadosComponent } from "./pages/certificados/certificados.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, PrimaryButtonComponent],
+  imports: [NavbarComponent, ItemCertificadoComponent, BaseUiComponent, CertificadosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificado-form',
+  imports: [],
+  templateUrl: './certificado-form.component.html',
+  styleUrl: './certificado-form.component.css'
+})
+export class CertificadoFormComponent {
+
+}
